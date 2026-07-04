e44e795ea5a875e9b4a6bdcd4ae830ddce8f99f3
ZeroJehovah <zj294753618@gmail.com>
2026-07-05 01:56:00 +0800

    Release bootstrap 0.4.509 remove empty runtime utility fragment

--- src/browser/runtime-utils-source.js
+++ src/browser/runtime-utils-source.js
@@ -4,11 +4,6 @@ function runtimeUtilityPreludeSource() {
   return "\n\n      const { safeStringify, safeJsonClone, sanitizeCombatLogIdPart } = require('./src/browser/runtime/runtime-utils');\n";
 }
 
-function runtimeUtilityCloneSource() {
-  return `\n\n`;
-}
-
 module.exports = {
-  runtimeUtilityPreludeSource,
-  runtimeUtilityCloneSource
+  runtimeUtilityPreludeSource
 };
